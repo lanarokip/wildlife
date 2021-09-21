96f613609665bb5dc739e70655035c362e658648
lanarokip <lankipkoecharo@gmail.com>
2021-09-21 19:54:40 +0300

    added the ranger section after droping the first table

--- create.sql
+++ create.sql
@@ -1,4 +1,6 @@
+SET MODE postgreSQL;
+
 CREATE DATABASE wildlife;
 \c wildlife;
-CREATE TABLE endangered(id SERIAL PRIMARY KEY,name VARCHAR,health VARCHAR,age VARCHAR,location VARCHAR);
+CREATE TABLE if NOT EXISTS endangered(id SERIAL PRIMARY KEY,name VARCHAR,health VARCHAR,age VARCHAR,location VARCHAR,ranger VARCHAR);
 CREATE DATABASE wildlife_test WITH TEMPLATE wildlife;
