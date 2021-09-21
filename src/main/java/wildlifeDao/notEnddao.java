3182516b42d848f7094a7f0ea181349888a7839e
lanarokip <lankipkoecharo@gmail.com>
2021-09-21 19:59:57 +0300

    added the dao section that declares the CRUDE method of the App

--- src/main/java/wildlifeDao/notEnddao.java
+++ src/main/java/wildlifeDao/notEnddao.java
@@ -1,4 +1,13 @@
 package wildlifeDao;
 
+import models.Not_endangered;
+
+import java.util.List;
+
 public interface notEnddao {
+    void create (Not_endangered not_endangered);
+    List<Not_endangered> findAll();
+    Not_endangered findById(int id);
+    void deleteAll();
+    void deleteById(int id);
 }
