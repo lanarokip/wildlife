0561292b1023167c7817ba16ee3a3205c8e73c14
lanarokip <lankipkoecharo@gmail.com>
2021-09-21 19:58:23 +0300

    added the class that holds the animals not endangerd

--- src/main/java/models/Not_endangered.java
+++ src/main/java/models/Not_endangered.java
@@ -1,4 +1,17 @@
 package models;
 
-public class not_endangered {
+public class Not_endangered extends Animal {
+
+    public Not_endangered(String name){
+        this.name=name;
+        this.id=id;
+    }
+
+    public String getName(){
+        return name;
+    }
+
+    public  void setId(int id){
+        this.id=id;
+    }
 }
