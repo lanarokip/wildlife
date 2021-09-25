a427818475fb0b45aa09d9d9fd48ec319b992faf
lanarokip <lankipkoecharo@gmail.com>
2021-09-25 08:50:42 +0300

    added some taste tasting the endangerd class if it instantiates the entries

--- src/test/java/wildlifeDao/RecEndangeredTest.java
+++ src/test/java/wildlifeDao/RecEndangeredTest.java
@@ -1,16 +1,36 @@
 package wildlifeDao;
 
+import models.EndangeredAnimals;
+import org.junit.Before;
 import org.junit.Test;
+import org.sql2o.Connection;
+import org.sql2o.Sql2o;
 
 import static org.junit.Assert.*;
 
 public class RecEndangeredTest {
+    RecEndangered recEndangered= new RecEndangered();
 
     @Test
-    public void create() {
+    public void createAnimal()throws Exception {
+        EndangeredAnimals endangeredAnimals= new EndangeredAnimals("aro","heatlth","young","secB","aro");
+        int myid = endangeredAnimals.getId();
+        recEndangered.create(endangeredAnimals);
+        assertNotEquals(myid,endangeredAnimals.getId());
+    }
+    @Test
+    public void createdAnimalCanBeFoundByfindAll() throws Exception {
+        EndangeredAnimals endangeredAnimals= new EndangeredAnimals("aro","heatlth","young","secB","aro");
+        recEndangered.create(endangeredAnimals);
+        EndangeredAnimals foundEndangered = recEndangered.findById(endangeredAnimals.getId());
+        assertEquals(endangeredAnimals,foundEndangered);
     }
 
-    @Test
-    public void findAll() {
-    }
+//    @Test
+//    public void create() {
+//    }
+
+//    @Test
+//    public void findAll() {
+//    }
 }
